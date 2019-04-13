e5715e294f9bca79a3ae76fb640cca4558af1d0c
stefaniemartins <stefanie.m@aluno.ifsc.edu.br>
2019-04-13 17:46:38 -0300

    Classes criadas.

--- src/main/java/poo/Soldado.java
+++ src/main/java/poo/Soldado.java
@@ -1,4 +1,26 @@
 package poo;
 
 public class Soldado {
+    public Soldado() {
+    }
+
+    public void movimentar () {
+
+    }
+
+    public void movimentar (int deslocamento) {
+
+    }
+
+    public boolean atacar ()
+    {
+
+    }
+
+    public boolean atacar (String arma)
+    {
+
+    }
+
+    
 }
